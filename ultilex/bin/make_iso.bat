ae5a28bfc3c7425484c82cc171a3c0c4fe8ff811
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2011-09-16 17:28:30 +0000

    -adding backbone ultilex

---
+++ ultilex/bin/make_iso.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+cd ..
+cd ..
+set CDLABEL=ULTILEX
+
+ultilex\stuff\tools\WIN\mkisofs.exe @ultilex\stuff\tools\WIN\config -o "../ultilex.iso" -A "%CDLABEL%" -V "%CDLABEL%" .
+echo.
+echo New ISO should be created now.
+goto theend
+
+:error1
+echo A parameter is required - target ISO file.
+echo Example: %0 c:\target.iso
+goto theend
+
+:error2
+echo Error creating the ISO file
+goto theend
+
+:theend
+pause
