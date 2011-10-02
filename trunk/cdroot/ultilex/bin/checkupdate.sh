e03ae225ee1e242d397e60afd5bfc89ed7248c50
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2011-10-02 13:04:19 +0000

    Modified linux scripts; Added all functionalities

---
+++ trunk/cdroot/ultilex/bin/checkupdate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "" ]; then
+	echo "Error"
+fi
+
+counter=0
+oldMeta=../../$1/meta
+newMeta=../temp/$1
+
+while IFS= read -r line; do
+	if [ "$counter" = "2" ]; then
+		oldMD5=$line
+	fi
+	counter=$(( counter + 1 ))
+done < $oldMeta/meta
+
+counter=0
+
+while IFS= read -r line; do
+	if [ "$counter" = "2" ]; then
+		newMD5=$line
+	fi
+	counter=$(( counter + 1 ))
+done < $newMeta/meta
+
+if [ $newMD5 = $oldMD5 ]; then
+	echo 0 > temp_eq.tmp
+else
+	echo 1 > temp_neq.tmp
+fi
+
+
