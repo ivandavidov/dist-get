c4f1c77b66b498db15d71f54c4bf8a290954d35b
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2011-10-15 17:26:56 +0000

    moved help and about information to separate files

--- trunk/cdroot/ultilex/bin/dist-get.bat
+++ trunk/cdroot/ultilex/bin/dist-get.bat
@@ -22,48 +22,7 @@ echo.
 goto end
 
 :help
-echo *** dist-get help page ***
-echo.
-echo Available options: 
-echo. 
-echo help                            Displays the current screen.
-echo.
-echo about                           Displays information about the authors of
-echo                                 this project.
-echo.
-echo prepare URL_META_FILE [local]   Downloads and prepares a metadata bundle for
-echo                                 certain distribution. The URL_META_FILE can
-echo                                 be HTTP/HTTPS location or a local file.
-echo                                 If it is a local file, you need to use the
-echo                                 'local' parameter at the end of the command.
-echo.
-echo list                            Displays a list of all prepared and installed
-echo                                 distributions.
-echo.                                
-echo install DISTRO_NAME [ISO_FILE]  Installs the DISTRO_NAME distribution. This
-echo                                 requires active internet connection unless
-echo                                 you provide a previously downloaded ISO file
-echo                                 as additional parameter.
-echo.
-echo update DISTRO_NAME              Updates the DISTRO_NAME distribution. Please
-echo                                 note that you have to retrieve the metadata
-echo                                 file fisrt by executing 'dist-get prepare'.
-echo.
-echo remove DISTRO_NAME              Removes the DISTRO_NAME distribution.
-echo.
-echo reorganize                      Builds the boot menu. This command is executed
-echo                                 automatically after install, update and remove.
-echo.
-echo cleanup [DISTRO_NAME]           Removes all temporary metadata. You can remove
-echo                                 meta data for DISTRO_NAME by providing it as
-echo                                 additional parameter. This command is executed
-echo                                 automatically before usbinstall or makeiso.
-echo.
-echo usbinstall                      Makes the current USB device to be bootable.
-echo.
-echo makeiso                         Creates a single CD ISO image which contains
-echo                                 all installed distributions.
-echo.
+type ..\stuff\help.txt | more
 goto end
 
 :prepare
@@ -75,15 +34,7 @@ call list
 goto end
 
 :about
-echo.
-echo *** ULTILEX powered by 'dist-get' ***
-echo.
-echo List of all contributors (in alphabet order):
-echo.
-echo Ivan Davidov, Sofia, Bulgaria (ivan.bgzin.com)
-echo.
-echo Peter Chakalov, Sliven, Bulgaria (peter.chakalov@gmail.com)
-echo.
+type ..\stuff\about.txt | more
 goto end
 
 :install
