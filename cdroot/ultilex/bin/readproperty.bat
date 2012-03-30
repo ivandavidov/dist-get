d442264af1d79c30e949ed9bd1eb21ff6d02e3ce
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2012-03-30 21:34:59 +0000

    Error messages are moved in separate (properties) file. Created a script for printing errors (for Windows version). A bug in readproperty.bat fixed.

--- cdroot/ultilex/bin/readproperty.bat
+++ cdroot/ultilex/bin/readproperty.bat
@@ -2,7 +2,7 @@
 
 ..\stuff\tools\WIN\properties\sed "/^\#/d" %1 | ..\stuff\tools\WIN\properties\grep %2 | ..\stuff\tools\WIN\properties\sed "s/^.*=//" > tmpfile.tmp
 
-for /f %%v in (tmpfile.tmp) do (
+for /f "tokens=* delims= " %%v in (tmpfile.tmp) do (
     echo %%v
     
     goto exitloop
