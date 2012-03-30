f0d49ff7dda758898fcc3f829c7de63d37d18b18
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2012-03-30 20:18:06 +0000

    New structure applied in cleanup and update [win versions only].

--- trunk/cdroot/ultilex/bin/cleanup.bat
+++ trunk/cdroot/ultilex/bin/cleanup.bat
@@ -4,11 +4,28 @@ if (%1) == () goto all
 goto custom
 
 :all
+REM *********************************
+REM Remove all temp directories BEGIN
+REM *********************************
 if exist ..\temp rmdir /s /q ..\temp
+
+cd ..\..\
+for /d %%v in (*) do (
+    if not "%%v"=="boot" (
+        if not "%%v"=="ultilex" (
+            if exist %%v\temp rmdir /s /q %%v\temp
+        )
+    )
+)
+cd .\ultilex\bin
+REM *******************************
+REM Remove all temp directories END
+REM *******************************
+
 goto end
 
 :custom
-if exist ..\temp\%1 rmdir /s /q ..\temp\%1
+if exist ..\..\%1\temp rmdir /s /q ..\..\%1\temp
 goto end
 
 :end
