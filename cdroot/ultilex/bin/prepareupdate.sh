02a492bd54eb8b657d2a8f258d967bea78c08d73
peter.chakalov@gmail.com <peter.chakalov@gmail.com@3e6dd940-baa7-0866-3df1-66e8398c036d>
2013-02-27 20:41:30 +0000

    Using the new functionality for printing errors

--- cdroot/ultilex/bin/prepareupdate.sh
+++ cdroot/ultilex/bin/prepareupdate.sh
@@ -2,24 +2,12 @@
 
 print_error()
 {
-	if [ "$1" = "1" ]; then
-		echo
-		echo "ERROR: You have already installed '$1'."
-		echo
-	elif [ "$1" = "2" ]; then
-		echo
-		echo "ERROR: Required parameter is missing. Use 'dist-get help' for more information."
-		echo
-	elif [ "$1" = "3" ]; then
-		echo
-		echo "ERROR: Metadata is missing or corrupted. Use 'dist-get cleanup $1' and then 'dist-get prepare'."
-		echo
-	fi
+	sh printerror.sh $1 $2
 	exit 1
 }
 
 if [ "$1" = "" ]; then
-	print_error 1
+	print_error missing_param
 fi
 
 temp=../../$1/temp
@@ -29,7 +17,11 @@ dir=../../$1
 # Check the meta structure BEGIN
 #********************************
 if [ ! -f $temp/meta -o ! -f $temp/includes -o ! -f $temp/menu.cfg -o ! -f $temp/add.cfg ]; then
-	print_error 2
+	print_error corrupted_old_meta_data $1
+fi
+
+if [ ! -f $dir/meta/meta -o ! -f $dir/meta/includes -o ! -f $dir/meta/menu.cfg -o ! -f $dir/meta/add.cfg ]; then
+	print_error corrupted_new_meta_data $1
 fi
 #******************************
 # Check the meta structure END
